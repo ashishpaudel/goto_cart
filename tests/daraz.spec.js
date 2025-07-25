27c1e36861af2719df832cf24a9bffa481b2fbcf
Aashish Poudel <aashish.poudel@ime.com.np>
2025-07-25 12:33:55 +0545

    go to cart is uploaded first time

---
+++ tests/daraz.spec.js
@@ -0,0 +1,18 @@
+import {expect, test} from '@playwright/test'
+import {login} from '../mop/login'
+import {Home, homePage} from '../mop/homePage'
+// method for login
+test('Daraz',async({page}) =>{ 
+    //login page  
+    const  Login = new login(page)
+    await Login.goToLoginpage(page)
+    await Login.login_page('ashishpoudel.2015@gmail.com' , 'Admin@123' )
+    
+    //home page 
+    const HomePage = new Home(page)
+    await HomePage.home('iphone')
+    // await page.getByAltText
+    await HomePage.addToCart('K9 Wireless Collar Mic iPhone/Android & Type C Supported Wireless Microphone')
+
+    // await HomePage.gotoCart()
+})
